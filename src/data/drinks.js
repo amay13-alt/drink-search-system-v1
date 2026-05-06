2f166bfbd8bed8a6be9ed532c446dd11e3fb0372
May <oturanno55@gmail.com>
2026-05-07 01:11:17 +0800

    Update V2: 實作關鍵字搜尋、品牌視覺化、加料篩選與程式碼重構

--- src/data/drinks.js
+++ src/data/drinks.js
@@ -3,55 +3,55 @@ export const mockDrinks = [
   { 
     id: 1, name: '珍珠紅茶', category: '紅茶', price: 40, store: '50嵐', 
     lat: 24.7971508936953, lng: 120.975997364986, toppings: ['珍珠'], rating: 4.2, 
-    logo: 'public/logos/50lan.jpg', 
+    logo: '/logos/50lan.jpg', 
     color: '#FFD700' 
   },
   { 
     id: 2, name: '翡翠綠茶', category: '綠茶', price: 35, store: '迷客夏', 
     lat: 24.7967686130477, lng: 120.975986636149, toppings: ['椰果'], rating: 4.0, 
-    logo: 'public/logos/milkshop.jpg', 
+    logo: '/logos/milkshop.jpg', 
     color: '#83A731' 
   },
   { 
     id: 3, name: '烏龍青茶', category: '青茶', price: 30, store: '可不可', 
     lat: 24.79657625482, lng: 120.975710368617, toppings: ['粉粿'], rating: 4.5, 
-    logo: 'public/logos/kebuke.png', 
+    logo: '/logos/kebuke.png', 
     color: '#003E52' 
   },
   { 
     id: 4, name: '拿鐵咖啡', category: '咖啡', price: 70, store: '可不可', 
     lat: 24.79657625482, lng: 120.975710368617, toppings: [], rating: 4.3, 
-    logo: 'public/logos/kebuke.png', 
+    logo: '/logos/kebuke.png', 
     color: '#003E52' 
   },
   { 
     id: 5, name: '厚奶茶', category: '奶茶', price: 60, store: '50嵐', 
     lat: 24.7971508936953, lng: 120.975997364986, toppings: ['布丁'], rating: 4.1, 
-    logo: 'public/logos/50lan.jpg', 
+    logo: '/logos/50lan.jpg', 
     color: '#FFD700' 
   },
   { 
     id: 6, name: '柳橙綠', category: '鮮果', price: 55, store: '麻古茶坊', 
     lat: 24.7979519748133, lng: 120.977153397072, toppings: [], rating: 4.4, 
-    logo: 'public/logos/macu.png', 
+    logo: '/logos/macu.png', 
     color: '#E61919' 
   },
   { 
     id: 7, name: '草莓冰沙', category: '鮮果', price: 65, store: '麻古茶坊', 
     lat: 24.7979519748133, lng: 120.977153397072, toppings: [], rating: 4.5, 
-    logo: 'public/logos/macu.png', 
+    logo: '/logos/macu.png', 
     color: '#E61919' 
   },
   { 
     id: 8, name: '炭燒青茶', category: '青茶', price: 60, store: '大茗本位', 
     lat: 24.7969171426064, lng: 120.976155615334, toppings: ['奶蓋'], rating: 4.2, 
-    logo: 'public/logos/daming.png', 
+    logo: '/logos/daming.png', 
     color: '#B06651' 
   },
   { 
     id: 9, name: '舞鶴紅茶', category: '紅茶', price: 60, store: 'MOREiN沫飲', 
     lat: 24.7971874172624, lng: 120.976257539268, toppings: ['仙草'], rating: 4.6, 
-    logo: 'public/logos/morein.png', 
+    logo: '/logos/morein.png', 
     color: '#010101' 
   }
 ];
